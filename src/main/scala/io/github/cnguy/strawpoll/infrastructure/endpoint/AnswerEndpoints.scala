547c927a25b4fb7f6d6c688274c02130cd80bf48
Chau <ChauTNguyen96@gmail.com>
2018-09-12 11:48:45 -0700

    refactor (API): Reduce nesting of AnswerEndpoints by ignoring exceptions

--- src/main/scala/io/github/cnguy/strawpoll/infrastructure/endpoint/AnswerEndpoints.scala
+++ src/main/scala/io/github/cnguy/strawpoll/infrastructure/endpoint/AnswerEndpoints.scala
@@ -4,7 +4,6 @@ import scala.language.higherKinds
 import cats.effect.Effect
 import io.circe.generic.auto._
 import io.circe.syntax._
-import io.github.cnguy.strawpoll.domain.{AnswerNotFoundError, PollNotFoundError}
 import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
@@ -35,59 +34,52 @@ class AnswerEndpoints[F[_]: Effect] extends Http4sDsl[F] {
     HttpService[F] {
       // TODO: Clean up this mess!
       case req @ PUT -> Root / "answers" / LongVar(id) =>
-        answerService.get(id).value.flatMap {
-          case Right(found) => {
-            pollService.get(found.pollId).value.flatMap {
-              case Right(found) => {
-                found.securityType match {
-                  case Some(IpAddressCheck) => {
-                    for {
-                      ipAddresses <- ipAddressService.list(id)
-                      _ = println("IpAddressCheck")
-                      exists = ipAddresses.exists(_.value == req.remoteAddr.getOrElse("null"))
-                      resp <- if (exists) {
-                        Forbidden("You cannot vote again!")
-                      } else {
-                        for {
-                          answer <- answerService.vote(id)
-                          _ <- ipAddressService.create(
-                            IpAddress(id, req.remoteAddr.getOrElse("null")))
-                          _ = println(req.remoteAddr)
-                          innerResp <- answer match {
-                            case Some(a) => Ok(a.copy(count = a.count + 1).asJson)
-                            case None => NotFound("The answer was not found.")
-                          }
-                        } yield innerResp
-                      }
-                    } yield resp
-                  }
-                  case Some(BrowserCookieCheck) => {
-                    // TODO
-                    for {
-                      answer <- answerService.vote(id)
-                      _ = println("BrowserCookieCheck")
-                      resp <- answer match {
-                        case Some(a) => Ok(a.copy(count = a.count + 1).asJson)
-                        case None => NotFound("The answer was not found.")
-                      }
-                    } yield resp
-                  }
-                  case _ => {
-                    for {
-                      answer <- answerService.vote(id)
-                      resp <- answer match {
-                        case Some(a) => Ok(a.copy(count = a.count + 1).asJson)
-                        case None => NotFound()
-                      }
-                    } yield resp
-                  }
+        for {
+          answer <- answerService.get(id).value
+          poll <- pollService.get(answer.right.get.pollId).value
+          resp <- poll.right.get.securityType match {
+            case Some(IpAddressCheck) => {
+              for {
+                ipAddresses <- ipAddressService.list(id)
+                _ = println("IpAddressCheck")
+                exists = ipAddresses.exists(_.value == req.remoteAddr.getOrElse("null"))
+                resp <- if (exists) {
+                  Forbidden("You cannot vote again!")
+                } else {
+                  for {
+                    answer <- answerService.vote(id)
+                    _ <- ipAddressService.create(IpAddress(id, req.remoteAddr.getOrElse("null")))
+                    _ = println(req.remoteAddr)
+                    innerResp <- answer match {
+                      case Some(a) => Ok(a.copy(count = a.count + 1).asJson)
+                      case None => NotFound("The answer was not found.")
+                    }
+                  } yield innerResp
                 }
-              }
-              case Left(PollNotFoundError) => NotFound("The poll was not found.")
+              } yield resp
+            }
+            case Some(BrowserCookieCheck) => {
+              // TODO
+              for {
+                answer <- answerService.vote(id)
+                _ = println("BrowserCookieCheck")
+                resp <- answer match {
+                  case Some(a) => Ok(a.copy(count = a.count + 1).asJson)
+                  case None => NotFound("The answer was not found.")
+                }
+              } yield resp
+            }
+            case _ => {
+              for {
+                answer <- answerService.vote(id)
+                resp <- answer match {
+                  case Some(a) => Ok(a.copy(count = a.count + 1).asJson)
+                  case None => NotFound()
+                }
+              } yield resp
             }
           }
-          case Left(AnswerNotFoundError) => NotFound("The answer was not found.")
-        }
+        } yield resp
     }
 
   def endpoints(
